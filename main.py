7008e2c317a688b019327f50985f7ab431a2cd9d
rehanchughtai97 <44495391+rehanchughtai97@users.noreply.github.com>
2020-08-08 15:13:08 +0100

    Comments added

--- main.py
+++ main.py
@@ -14,10 +14,14 @@ def conversionCalculator():
     ans = inches * 12
     print("Your height is " , math.trunc(feet) ,"ft", round(ans), "inches")
 
+#Converting feet and inches into cm
 def feetConversion():
+    #Take user input for feet and inches
     feet = int(input("Input your height in feet: "))
     inches = int(input("Input your height in inches: "))
+    #Inches and feet multiplied by 12 
     inches += feet * 12
+    #CM rounds inches by 2.54 to give final value
     cm = round(inches * 2.54, 1)
     print("Your height is: %d cm." % cm)
 
