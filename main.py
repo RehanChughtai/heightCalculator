8b9add98f6558f732a7a07197292911708c88636
rehanchughtai97 <44495391+rehanchughtai97@users.noreply.github.com>
2020-08-08 15:06:02 +0100

    Fixed inches issue using remainder

--- main.py
+++ main.py
@@ -1,11 +1,17 @@
+#Imported to math.trunc to remove decimal places
 import math
 
+#Converts value from cm to ft and inches
 def conversionCalculator():
     print("Welcome to the height calculator!")
     print("Convert CM to Feet and Inches")
     cm = int(input("Input a number in cm format: "))
+    #Gives answer in ft
     feet = 0.0328084 * cm
-    inches = 0.393701 * cm
-    print("Your height is " , math.trunc(feet) ,"ft", round(inches,2), "inches")
+    #Takes remainder of ft after decimal place
+    inches = feet % 1
+    #Multiply by 12 to get the remainder of feet
+    ans = inches * 12
+    print("Your height is " , math.trunc(feet) ,"ft", round(ans), "inches")
 
 conversionCalculator()
